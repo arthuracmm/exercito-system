8c6849cb4933a942ca31238a92e1e01fb7e121a6
Arthur Cesar <arthurcesarmarcelino@gmail.com>
2025-04-10 15:12:14 -0300

    feat: add new declaration templates and enhance PDF rendering in Declaracao page

--- src/pages/Declaracao.tsx
+++ src/pages/Declaracao.tsx
@@ -9,6 +9,22 @@ export function Declaracao() {
     const [semanaSelecionada, setSemanaSelecionada] = useState<string>('current');
     const [diasDaSemana, setDiasDaSemana] = useState<any[]>([]);
 
+    const declaracaoSemanalAtirador: string = "Declaramos para os devidos fins que o <strong>Atirador {atirador?.nomeatr.toUpperCase()}</strong> esteve em cumprimento de missões militares (INSTRUÇÃO SEMANAL) no TIRO DE GUERRA 02-013 (FRANCA-SP), sito à Av. Distrito Federal, 1010 - Vila Aparecida - CEP: 14.401-342, nesta cidade de Franca-SP, <strong>{texto}</strong>, conforme amparo na Constituição da República Federativa do Brasil de 1988 e Lei N° 4.375, de 17 AGO 64 (Lei do Serviço Militar).";
+    
+    const declaracaoSemanalMonitor: string = "Declaro para os devidos fins, que o <strong>Monitor {atirador?.nomeatr.toUpperCase()},</strong> esteve em cumprimento de missões militares (INSTRUÇÃO SEMANAL) no TIRO DE GUERRA 02-013 (FRANCA-SP), sito à Av. Distrito Federal, 1010 - Vila Aparecida - CEP: 14.401-342, nesta cidade de Franca-SP, <strong>{texto}</strong>, conforme amparo na Constituição da República Federativa do Brasil de 1988 e Lei nº 4.375, de 17 AGO 64 (Lei do Serviço Militar).";
+
+    const declaracaoFaculdadeEmpresa: string = "Declaro para os devidos fins, que o Atirador <strong>Atirador {atirador?.nomeatr.toUpperCase()},</strong>, está matriculado no TIRO DE GUERRA 02-013 (FRANCA), e prestará o Serviço Militar Inicial Obrigatório, {dataPersonalizada}.";
+
+    const declaracaoMissaoEscala: string = "Declaro para os devidos fins, que o Atirador <strong>{atirador?.nomeatr.toUpperCase()}</strong>, esteve em cumprimento de missões militares no TIRO DE GUERRA 02-013 (FRANCA-SP), sito à Av. Distrito Federal, 1010 - Vila Aparecida - CEP: 14.401-342, nesta cidade de Franca-SP, <strong>{dataPersonalizada}</strong> conforme amparo na Constituição da República Federativa do Brasil de 1988 e Lei nº 4.375, de 17 AGO 64 (Lei do Serviço Militar).";
+
+    const declaracaoMissaoExternaEscala: string = "Declaro para os devidos fins, que o Atirador <strong>{atirador?.nomeatr.toUpperCase()}</strong>, esteve em cumprimento de missões militares no(a) , <strong>{dataPersonalizada}</strong> conforme amparo na Constituição da República Federativa do Brasil de 1988 e Lei nº 4.375, de 17 AGO 64 (Lei do Serviço Militar)."
+
+    const declaracaoMissaoExternaVoluntario: string = "Declaro para os devidos fins, que o Atirador <strong>{atirador?.nomeatr.toUpperCase()}</strong>, esteve voluntariamente em cumprimento de missões militares no(a) , <strong>{dataPersonalizada}</strong> conforme amparo na Constituição da República Federativa do Brasil de 1988 e Lei nº 4.375, de 17 AGO 64 (Lei do Serviço Militar)."
+
+    const declaracaoMissaoVoluntaria: string = "Declaro para os devidos fins, que o Atirador <strong>{atirador?.nomeatr.toUpperCase()}</strong>, esteve voluntariamente em cumprimento de missões neste TIRO DE GUERRA 02-013 (FRANCA-SP), sito à Av. Distrito Federal, 1010 - Vila Aparecida - CEP: 14.401-342, nesta cidade de Franca-SP <strong>{dataPersonalizada}</strong>."
+
+    const declaracaoServico: string = "Declaro para os devidos fins, que o Atirador <strong>{atirador?.nomeatr.toUpperCase()}</strong>, esteve em cumprimento de missões militares (MISSÃO DE GUARDA DO AQUARTELAMENTO) pelo TG 02-013 <strong>{dataPersonalizada}</strong>. do corrente ano, amparado pelo Art. 143 da Constituição Federal e o constante na Lei nº 4.375, de 17 AGO 64 (Lei do Serviço Militar)"
+
     // Carregar a lib html2pdf dinamicamente
     useEffect(() => {
         const script = document.createElement('script');
@@ -140,47 +156,139 @@ export function Declaracao() {
                 </button>
 
                 {/* PDF invisível para renderização */}
-                <div id="pdf-content" style={{ display: "none", padding: "40px", fontFamily: "serif" }}>
+                <div id="pdf-content" style={{ display: "none", fontFamily: "serif" }}>
                     {selectedAtiradores.map((id) => {
-                        const atirador = atiradores.find(a => a.id === id);
+                        const atirador = atiradores.find((a) => a.id === id);
                         const texto = gerarTextoPresencas(id);
+
+                        const htmlDeclaracao = `
+      Declaramos para os devidos fins que o(a) atirador(a) 
+      <strong>${atirador?.nomeatr.toUpperCase()}</strong> esteve em cumprimento de missões militares (INSTRUÇÃO SEMANAL) no TIRO DE GUERRA 02-013 (FRANCA-SP), 
+      sito à Av. Distrito Federal, 1010 - Vila Aparecida - CEP: 14.401-342, nesta cidade de Franca-SP, 
+      <strong>${texto}</strong>, conforme amparo na Constituição da República Federativa do Brasil de 1988 e Lei N° 4.375, de 17 AGO 64 (Lei do Serviço Militar).
+    `;
+
                         return (
-                            <div key={id} style={{ pageBreakAfter: 'always', marginBottom: '60px' }}>
-                                <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', width: '100%' }}>
-                                    <img src="public/images/tg-icon.png" alt="icone tg" width={60} />
-                                    <div style={{ textAlign: 'center', fontSize: '12px' }}>
-                                        <p><strong style={{ margin: '2px 0' }}>MINISTÉRIO DA DEFESA</strong></p>
-                                        <p><strong style={{ margin: '2px 0' }}>EXÉRCITO BRASILEIRO</strong></p>
-                                        <p><strong style={{ margin: '2px 0' }}>COMANDO MILITAR DO SUDESTE</strong></p>
-                                        <p><strong style={{ margin: '2px 0' }}>COMANDO MILITAR DA 2ª REGIÃO MILITAR</strong></p>
-                                        <p style={{ margin: '2px 0' }}>(Cmdo das Armas Prov PR/1890)</p>
-                                        <p><strong style={{ margin: '2px 0' }}>"REGIÃO DAS BANDEIRAS"</strong></p>
-                                        <p><strong style={{ margin: '2px 0' }}>TIRO DE GUERRA 02-013 (FRANCA-SP)</strong></p>
-                                        <p><strong style={{ margin: '2px 0' }}>Aislan Alves Moreira</strong></p>
+                            <div
+                                key={id}
+                                style={{
+                                    display: 'flex',
+                                    flexDirection: 'column',
+                                    justifyContent: 'space-between',
+                                    alignItems: 'center',
+                                    height: '1120px',
+                                    width: '100%',
+                                    padding: '40px',
+                                    boxSizing: 'border-box',
+                                    pageBreakAfter: 'always',
+                                    position: 'relative',
+                                    zIndex: 1,
+                                    overflow: 'hidden',
+                                }}
+                            >
+                                {/* Imagem de fundo (marca d'água) */}
+                                <img
+                                    src="public/images/tg-icon.png"
+                                    alt="marca d'água"
+                                    style={{
+                                        position: 'absolute',
+                                        top: '50%',
+                                        left: '50%',
+                                        transform: 'translate(-50%, -50%)',
+                                        width: '80%',
+                                        height: 'auto',
+                                        opacity: 0.1,
+                                        zIndex: 0,
+                                        pointerEvents: 'none',
+                                    }}
+                                />
+
+                                {/* Cabeçalho */}
+                                <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', width: '80%', zIndex: 1 }}>
+                                    <img src="public/images/exercito-icon.png" alt="icone exercito" width={70} />
+                                    <div style={{ textAlign: 'center', fontSize: '15px' }}>
+                                        <p><strong>MINISTÉRIO DA DEFESA</strong></p>
+                                        <p><strong>EXÉRCITO BRASILEIRO</strong></p>
+                                        <p><strong>COMANDO MILITAR DO SUDESTE</strong></p>
+                                        <p><strong>COMANDO MILITAR DA 2ª REGIÃO MILITAR</strong></p>
+                                        <p>(Cmdo das Armas Prov PR/1890)</p>
+                                        <p><strong>"REGIÃO DAS BANDEIRAS"</strong></p>
+                                        <p><strong>TIRO DE GUERRA 02-013 (FRANCA-SP)</strong></p>
                                     </div>
-                                    <img src="public/images/tg-icon.png" alt="icone tg" width={60} />
+                                    <img src="public/images/tg-icon.png" alt="icone tg" width={100} />
                                 </div>
 
-                                <h1 style={{ fontSize: '40px', marginTop: '40px', textAlign: 'center', fontWeight: 'bold' }}>
-                                    DECLARAÇÃO
-                                </h1>
+                                {/* Corpo da declaração */}
+                                <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', width: '100%' }}>
+                                    <h1 style={{ fontSize: '40px', marginTop: '40px', textAlign: 'center', fontWeight: 'bold' }}>DECLARAÇÃO</h1>
 
-                                <p style={{ fontSize: '12px', marginTop: '80px', lineHeight: '1.6', textAlign: 'justify', textIndent: '30px' }}>
-                                    Declaramos para os devidos fins que o(a) atirador(a) <strong>{atirador?.nomeatr.toUpperCase()}</strong> esteve em cumprimento de missões militares (INSTRUÇÃO SEMANAL) no TIRO DE GUERRA 02-013 (FRANCA-SP), sito à Av. Distrito Federal, 1010 - Vila Aparecida - CEP: 14.401-342, nesta cidade de Franca-SP, <strong>{texto}</strong>, conforme amparo na Constituição da República Federativa do Brasil de 1988 e Lei N° 4.375, de 17 AGO 64 (Lei do Serviço Militar).
-                                </p>
+                                    <p
+                                        style={{
+                                            fontSize: '15px',
+                                            marginTop: '80px',
+                                            lineHeight: '1.6',
+                                            textAlign: 'justify',
+                                            textIndent: '30px',
+                                            zIndex: 1
+                                        }}
+                                        dangerouslySetInnerHTML={{ __html: htmlDeclaracao }}
+                                    />
 
-                                <p style={{ fontSize: '12px', fontStyle: 'italic', marginTop: '40px', lineHeight: '1.5', textAlign: 'justify', textIndent: '30px' }}>
-                                    Esta declaração só terá validade sendo ORIGINAL com MARCA D'ÁGUA e assinada apenas pelo <strong>Subtenente AISLAN ALVES MOREIRA</strong>. As declarações diferentes deste modelo não terão validade, incluindo a reprodução de CÓPIAS.
-                                </p>
+                                    <p style={{
+                                        fontSize: '15px',
+                                        fontStyle: 'italic',
+                                        marginTop: '40px',
+                                        lineHeight: '1.5',
+                                        textAlign: 'justify',
+                                        textIndent: '30px',
+                                        zIndex: 1
+                                    }}>
+                                        Esta declaração só terá validade sendo ORIGINAL com MARCA D'ÁGUA e assinada apenas pelo <strong>Subtenente AISLAN ALVES MOREIRA</strong>. As declarações diferentes deste modelo não terão validade, incluindo a reprodução de CÓPIAS.
+                                    </p>
 
-                                <div style={{ display: 'flex', flexDirection: 'column', justifyContent: 'space-between', alignItems: 'center', width: '100%', marginTop: '80px' }}>
-                                    <p style={{ fontSize: '12px', marginTop: '4px' }}> <strong>AISLAN ALVES MOREIRA - Subtenente </strong></p>
-                                    <p style={{ fontSize: '12px' }}>Chefe de Instrução Tiro de Guerra 02-013</p>
+                                    <div style={{
+                                        display: 'flex',
+                                        flexDirection: 'column',
+                                        alignItems: 'center',
+                                        width: '100%',
+                                        marginTop: '80px',
+                                        zIndex: 1
+                                    }}>
+                                        <p style={{ fontSize: '15px', marginTop: '4px' }}><strong>AISLAN ALVES MOREIRA - Subtenente</strong></p>
+                                        <p style={{ fontSize: '15px' }}>Chefe de Instrução Tiro de Guerra 02-013</p>
+                                    </div>
+                                </div>
+
+                                {/* Rodapé */}
+                                <div style={{ display: 'flex', flexDirection: 'column', width: '100%', marginTop: '80px', zIndex: 1 }}>
+                                    <div style={{ width: '100%', height: '1px', backgroundColor: 'black' }} />
+                                    <p style={{ fontSize: '12px' }}><strong>Tiro de Guerra 02-013 (Franca-SP)</strong></p>
+                                    <p style={{ fontSize: '12px' }}>Av. Distrito Federal, 1010 - Vila Aparecida - CEP: 14.401-342 - Franca-SP</p>
+
+                                    <div style={{ display: 'flex', justifyContent: 'space-between', width: '100%' }}>
+                                        <div style={{ display: 'flex', gap: '5px' }}>
+                                            <img src="public/images/whatsapp-icon.png" alt="whatsapp" style={{ width: 15, height: 'auto' }} />
+                                            <p style={{ fontSize: '12px' }}>(16) 3723-9321</p>
+                                        </div>
+
+                                        <div style={{ display: 'flex', gap: '5px' }}>
+                                            <img src="public/images/at-icon.png" alt="email" style={{ width: 15, height: 'auto' }} />
+                                            <p style={{ color: 'blue', textDecoration: 'underline', fontSize: '12px' }}>franca.tg02013@gmail.com</p>
+                                        </div>
+
+                                        <div style={{ display: 'flex', gap: '5px' }}>
+                                            <img src="public/images/instagram-icon.png" alt="instagram" style={{ width: 15, height: 'auto' }} />
+                                            <p style={{ color: 'blue', textDecoration: 'underline', fontSize: '12px' }}>
+                                                https://www.instagram.com/tirodeguerra_02013/
+                                            </p>
+                                        </div>
+                                    </div>
                                 </div>
                             </div>
                         );
                     })}
                 </div>
+
 
             </div>
         </div>
